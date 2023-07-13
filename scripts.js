ff4e2c21a393391fa12c33ec37c6d7e02c1afcbc
Rishikesh <krishikesh369@gmail.com>
2023-07-14 02:00:13 +0530

    added skills

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// const TagCloudMin = require("./TagCloud.min");
 
 
 
@@ -22,4 +23,35 @@ navList.forEach((element) =>{
         navBar.style.transform = "translateY(-150%)";
         navAdd.style.display = "block"
     });
+})
+
+
+//Skill add
+const skill =[
+    "Front-end Development",
+    "React.Js",
+    "Redux",
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "Java",
+    "Git & Github",
+    "Data Structure & Algorithms",
+    "Problem Solving",
+    "Node Js",
+    "MongoDB",
+    "OOPs",
+    "Express",
+]
+
+var tagcloud = TagCloud(".sk", skill,{
+    radius: 280, // rolling radius, unit `px`
+    maxSpeed: 'normal', // rolling max speed, optional: `slow`, `normal`(default), `fast`
+    initSpeed: 'fast', // rolling init speed, optional: `slow`, `normal`(default), `fast`
+    direction: 225, // rolling init direction, unit clockwise `deg`, optional: `0`(top) , `90`(left), `135`(right-bottom)(default)...
+    keep: true, // whether to keep rolling after mouse out area, optional: `false`, `true`(default)(decelerate to rolling init speed, and keep rolling with mouse)
+    useContainerInlineStyles: true,
+    useItemInlineStyles: true,
+    containerClass: 'tagcloud',
+    itemClass: 'tagcloud--item'
 })
